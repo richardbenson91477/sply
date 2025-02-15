6b39809b4b0aa507ee1e23f1ef57701a5bab891e
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-15 02:52:59 -0600

    usability improvements

--- sp_test_edit.py
+++ sp_test_edit.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
-import sply
 import random
+import sply
 
 def main ():
+    model_id = "default-code"
+
+    if len(sys.argv) > 1:
+        for argv in sys.argv[1:]:
+            if argv.find("model_id=") == 0:
+                model_id = argv[9:]
+
     options = {
         "seed": random.randint(0, 2 << 32),
         "temperature": 0.0,
         "num_ctx": 1_000,
         }
 
-    sp = sply.sp(show=True, options=options)
+    sp = sply.sp(show=True, model_id=model_id, options=options)
 
     sp.runcode("y = 1", show=True)
     s = sp.runcode("y + 1", show=True)
     print(f"**{s}**")
 
-    s.edit_prompt(show=True)
+    sp.edit_prompt(show=True)
 
 if __name__ == "__main__":
     exit(main())
