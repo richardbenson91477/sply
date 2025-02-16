982a33b12243b6ae8eab1d9a90ea9be794760a64
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-16 12:29:53 -0600

    shuffled some files around

---
+++ node_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import overmind
+
+n = overmind.node("n", "a primary node")
+
+#print(n("what is the overmind?"))
+
+jane = n.new("jane", "a lady named Jane")
+
+jane["name"] = "Jane"
+
+print(jane["name"])
+
+jane("hello!")
+
+#jane += n.new("snacks", "some delicious snacks")
+
--- overmind/README.md
+++
@@ -1,6 +0,0 @@
-* The Overmind
-
-an experiment in abstract LLM control using the SPLY library
-
-(early proof of concept)
-
