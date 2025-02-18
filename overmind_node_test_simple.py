ff4eeca61ca950eff7e5ae9ad09d84e75103739a
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-18 08:32:39 -0600

    better names for node tests

---
+++ overmind_node_test_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import overmind
+
+n = overmind.node("n", "a primary node, connected to the overmind network")
+
+print(n["how many nodes are active?"])
+
+jane = n.new("jane", "a lady named Jane")
+
+jane["name"] = "Jane"
+
+print(jane["name"])
+
+snacks = n.new("snacks", "some delicious snacks")
+
+print(n("give the 'snacks' to 'jane'"))
+
+print(jane("how are you?"))
+
