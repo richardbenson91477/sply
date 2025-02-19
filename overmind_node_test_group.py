ad3f4351f9b124f12ffb13acf10156ee5dc60556
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-19 02:18:49 -0600

    minor phrasing improvements

--- overmind_node_test_group.py
+++ overmind_node_test_group.py
@@ -4,7 +4,7 @@ import overmind
 n = overmind.node("n", "a primary node connected to the Overmind")
 jane = n.node("jane", "an intelligent being named Jane, female gender, age 25.")
 jane["name"]
-n("infer the meaning of a 'realism' property of the jane object and describe it to me")
+n("infer the meaning of the 'realism' property of the 'jane' node and describe it to me")
 jane["realism"] = "very high"
 jane["IQ"] = "120.0"
 user = n.node("user", "a reference to myself, a man named John")
@@ -12,7 +12,7 @@ jane["master"] = "user"
 jane("how are you feeling today?")
 sue = n.node("sue", "Sue - a copy of Jane with various personal properties randomly changed by up to 20%")
 sue["master"] = "user"
-g = n.node("g", "an empty group that I can communicate with")
+g = n.node("g", "an uninitialized node group")
 g["members"] = ["jane", "sue"]
 room = n.node("room", "a large and luxurious living room")
 user["location"] = "room"
--- overmind_node_test_simple.py
+++ overmind_node_test_simple.py
@@ -12,5 +12,5 @@ print(jane["name"])
 snacks = n.node("snacks", "some delicious snacks")
 print(n("give the 'snacks' to 'jane'"))
 
-print(jane("how are you?"))
+print(jane("What's up?"))
 
