f2bf1c5f8c125b0ef4e6d2ad8cc9473e0e217200
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-20 06:17:45 -0600

    remove a debug print

--- sply/chat.py
+++ sply/chat.py
@@ -103,8 +103,6 @@ class chat:
                 else self.default_args["num_ctx"],
             }
 
-        print(f"seed = {self.options["seed"]}")
-
     def edit_prompt (self):
         prompt_file = tempfile.mktemp()
 
