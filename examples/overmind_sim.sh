24acff2687553ce8123e5c6249e5493ab736384e
Richard A. Benson <richardbenson91477@protonmail.com>
2025-02-24 18:03:15 -0600

    locate prompt by path of bin

--- examples/overmind_sim.sh
+++ examples/overmind_sim.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+
+
 rev_prompt=$(printf "\n>>> ")
 
 sply_chat_interact.py \
   in_suffix_enabled="False" \
   rev_prompt="$rev_prompt" \
-  prompt_file="examples/overmind_sim.prompt" \
+  prompt_file="$(dirname $(realpath $0))/overmind_sim.prompt" \
   seed="$RANDOM" \
   temp="0.8" \
   num_ctx="8_000" \
