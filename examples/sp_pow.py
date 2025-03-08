70db602dbba3e6a36580fbb40090932d3d58a64d
Richard A. Benson <richardbenson91477@protonmail.com>
2025-03-08 14:12:57 -0600

    simple math test - some models fail! list update next

---
+++ examples/sp_pow.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+import sply
+
+
+def main ():
+    model_id = "default-code"
+
+    for argv in sys.argv[1:]:
+        if argv.find("model_id=") == 0:
+            model_id = argv[9:]
+
+    num_ctx = 1_000
+
+    sp = sply.sp(
+        show=True,
+        model_id=model_id,
+        num_ctx=num_ctx,
+        )
+
+    sp.runcode("y = 3 + 2")
+    s = sp.runcode("y ** 3")
+    print(f"**{s}**")
+
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
+
+
