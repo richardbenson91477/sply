52fc6caa6cd1fded5ac11c03174b33bb43faf768
Richard A. Benson <richardbenson91477@protonmail.com>
2025-03-11 00:23:48 -0500

    experiment with <think>

---
+++ examples/sp_pow_think.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+import sply
+
+
+def main ():
+    model_id = "default-code"
+    seed = ""
+    temp = 0.0
+    num_ctx = 1_000
+
+    for argv in sys.argv[1:]:
+        if argv.find("model_id=") == 0:
+            model_id = argv[9:]
+        elif argv.find("seed=") == 0:
+            seed = int(argv[5:])
+        elif argv.find("temp=") == 0:
+            temp = float(argv[5:])
+        elif argv.find("num_ctx=") == 0:
+            num_ctx = int(argv[8:])
+
+    sp = sply.sp(
+        show=True,
+        model_id=model_id,
+        num_ctx=num_ctx,
+        temp=temp,
+        seed=seed,
+        )
+
+    sp.runcode("i = 3 + 2")
+    sp.runcode_think("print (i ** 3)")
+#    sp.runcode("print (pow (i, 3))")
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
+
+
