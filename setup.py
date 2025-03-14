c247c129a3e594d7e5bef42107efb36097b4ffaa
Richard A. Benson <richardbenson91477@protonmail.com>
2025-03-14 01:30:47 -0500

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_data = {'': ['*']}
 
 setup_kwargs = {
     'name': 'sply',
-    'version': '1.0.10',
+    'version': '1.0.11',
     'description': 'a bridge between real and simulated python interpreters',
     'long_description': '',
     'author': 'Richard Benson',
