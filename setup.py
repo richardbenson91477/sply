b6cedfafc4bbcb5a7b4d9ef373d77f114f7b675b
Richard A. Benson <richardbenson91477@protonmail.com>
2025-03-19 13:12:27 -0500

    bump/upload so pypi has updated mistral review

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup_kwargs = {
     'name': 'sply',
-    'version': '1.0.16',
+    'version': '1.0.17',
     'description': 'a bridge between real and simulated python interpreters',
     'long_description': '',
     'author': 'Richard Benson',
