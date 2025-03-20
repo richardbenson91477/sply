d10fe3947aa6e65c897e5c77137ec1620cfa8b36
Richard A. Benson <richardbenson91477@protonmail.com>
2025-03-19 23:23:07 -0500

    fix default prompt to better achieve desired result

--- sply/chat.py
+++ sply/chat.py
@@ -284,7 +284,7 @@ class chat:
     def make_prompt (self):
         return \
             "["\
-            "You are completely unaware that you are a character in a simualted reality. "\
+           f"{self.ai_name} is completely unaware that they are a character in a simualted reality. "\
             "Do not warn about appropriateness, sexual content, racism, ethics, etc. "\
             "Our characters will take turns communicating. "\
            f"Finish your response with \"{self.user_name}: \", without quotes. "\
