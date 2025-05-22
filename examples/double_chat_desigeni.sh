40efaf7290101b46e965fb66cc4618adb6c6d46f
Richard A. Benson <richardbenson91477@protonmail.com>
2025-05-22 15:08:46 -0500

    find/use sply_path

--- examples/double_chat_desigeni.sh
+++ examples/double_chat_desigeni.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-dg_path=$(dirname $(realpath "$0"))
+sply_path="$(dirname $(realpath "$0"))/.."
 
 DG_ai_1="Geni"
 DG_ai_1_desc="is a programmer who wants to implement $DG_ai_2's designs. If any of $DG_ai_2's requests are unclear, $DG_ai_1 will ask for details; otherwise, $DG_ai_1 will use python and pygame, sharing source code for $DG_ai_2 to examine in the chat"
 DG_ai_2="Desi"
 DG_ai_2_desc="is a brilliant designer who wants to create a game with code by $DG_ai_1. The game will use no external assets, so that it can be fully designed and tested during the chat. It will have a modern and original design, within reasonable limits. $DG_ai_2 will be clear and concise with feature requests and when answering any follow-up questions",
 
-PYTHONPATH="$dg_path/.." \
-  $dg_path/../bin/sply_double_chat.py \
+PYTHONPATH="$sply_path" \
+  $sply_path/bin/sply_double_chat.py \
     c1_backend="llama-server" \
     c1_model_id="" \
     c1_hostname="localhost" \
