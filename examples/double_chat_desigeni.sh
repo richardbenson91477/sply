9da8d0caece10e13c5e51bf279131a6eb4f63982
Richard A. Benson <richardbenson91477@protonmail.com>
2025-05-23 16:53:02 -0500

    fix variable decl. order

--- examples/double_chat_desigeni.sh
+++ examples/double_chat_desigeni.sh
@@ -3,9 +3,10 @@
 sply_path="$(dirname $(realpath "$0"))/.."
 
 export ai1="Geni"
+export ai2="Desi"
+
 export ai1_desc="is a programmer who wants to implement $ai2's designs. If any of $ai2's requests are unclear, $ai1 will ask for details; otherwise, $ai1 will use python and pygame, sharing source code for $ai2 to examine in the chat"
 
-export ai2="Desi"
 export ai2_desc="is a brilliant designer who wants to create a game with code by $ai1. The game will use no external assets, so that it can be fully designed and tested during the chat. It will have a modern and original design, within reasonable limits. $ai2 will be clear and concise with feature requests and when answering any follow-up questions"
 
 $sply_path/examples/double_chat_desigeni.sh
--- examples/double_chat_desipeng.sh
+++ examples/double_chat_desipeng.sh
@@ -3,9 +3,10 @@
 sply_path="$(dirname $(realpath "$0"))/.."
 
 export ai1="Peng"
+export ai2="Desi"
+
 export ai1_desc="is a prompt engineer who wants to form $ai2's designs into a programming prompt for an LLM. If any of $ai2's requests are not entirely clear, $ai1 will ask for details; otherwise, $ai1 will use markdown, sharing specs for $ai2 to review in the chat"
 
-export ai2="Desi"
 export ai2_desc="wants to design a single-file pygame using a prompt crafted by $ai1. It will have a modern and original design. $ai2 will try to be clear and concise with feature requests and when asking or answering follow-up questions"
 
 $sply_path/examples/double_chat_examples.sh
