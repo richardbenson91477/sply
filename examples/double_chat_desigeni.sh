412a711af0d8b84a4b414310940d1672c7af35f6
Richard A. Benson <richardbenson91477@protonmail.com>
2025-05-26 19:51:08 -0500

    consist.

--- examples/double_chat_desigeni.sh
+++ examples/double_chat_desigeni.sh
@@ -5,10 +5,10 @@ sply_path="$(dirname $(realpath "$0"))/.."
 export ai1="Geni"
 export ai2="Desi"
 
-export ai1_desc_ai1="is a programmer who wants to implement $ai2's designs. If any of $ai2's requests are unclear, $ai1 will ask for details; otherwise, $ai1 will use python and pygame, sharing source code for $ai2 to examine in the chat"
+export ai1_desc_ai1="is a programmer who wants to implement $ai2's designs. If any of $ai2's requests are unclear, $ai1 will ask for details; otherwise, $ai1 will use python, sharing source code for $ai2 to examine in the chat"
 export ai2_desc_ai1="$ai1_desc_ai1"
 
-export ai2_desc_ai2="is a brilliant designer who wants to create a game with code by $ai1. The game will use no external assets, so that it can be fully designed and tested during the chat. It will have a modern and original design, within reasonable limits. $ai2 will be clear and concise with feature requests and when answering any follow-up questions"
+export ai2_desc_ai2="wants to design a single-file pygame (no external assets) with code by $ai1. It will have an original design. $ai2 will be clear and concise with feature requests and when asking or answering follow-up questions"
 export ai1_desc_ai2="$ai2_desc_ai2"
 
 $sply_path/examples/double_chat_examples.sh
